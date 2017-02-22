a8b6cff23326ac7b3a930d8ed61b46cf9b000990
andrewdc92 <andrew.cordivari@gmail.com>
2017-02-21 17:26:59 -0800

    adds custom middleware

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log');
+    }
+  });
+  next();
+});//next tells middleware when it's done, if it's not CALLED the handlers will not fire
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -35,6 +56,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is up and running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
 });
